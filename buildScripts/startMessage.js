f08070e431a020decba7801b19ffe7f8ccc4e67f
omadoyeabraham <omadoyeabraham@gmail.com>
2017-04-30 13:03:25 +0100

    integrated mocha and chai for testing

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -2,6 +2,6 @@
  * Displays a start message whenever the app starts
  */
 
-const chalk = require('chalk');
+import chalk from 'chalk';
 
-console.log(chalk.green('Starting app in dev mode ...'));
+console.log(chalk.green('Starting app in dev mode ...')); //eslint-disable-line no-console
